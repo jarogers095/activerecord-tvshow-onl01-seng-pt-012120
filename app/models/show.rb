e9f81f26279121c3f766f23a12355d1fcdf95805
James Rogers <jarogers095@gmail.com>
2020-03-30 05:12:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -10,12 +10,12 @@ class Show < ActiveRecord::Base
   
   
   def self.lowest_rating()
-    
+    Show.minimum(:rating)
   end
   
   
   def self.least_popular_show()
-    
+    Show.find_by(rating: self.lowest_rating)
   end
   
   
